87897d2f7d7d10e6c952b5941e06d80982712f75
rodrigofull <cabelodenovo@gmail.com>
2025-10-21 09:28:24 -0300

    imagem inserida

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,18 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Videos from './pages/Videos';
+import file from './assets/file.png';
 
 function App() {
   return (
     <Router>
       <Navbar />
+
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <h1>Minha imagem no React</h1>
+        <img src={file} alt="Minha imagem" width="200" />
+      </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
